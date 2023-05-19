ebcea251083bddf2cb0277061e156249f47a643c
Eugene <75242911+EugenSong@users.noreply.github.com>
2023-05-19 02:32:40 -0700

    Generated uuid for create game function. Managed to store uuid and new 19x19 board into firestore by converting array -> js object.
    Firestore transformed object into map in its database.

--- src/pages/backend/firebase.js
+++ src/pages/backend/firebase.js
@@ -1,11 +1,16 @@
 // Import the functions you need from the SDKs you need
 
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 //import { getFunctions } from "firebase/functions";
 import { collection, addDoc } from "firebase/firestore";
 
+import { v4 as uuidv4 } from 'uuid';
+
+const BOARD_LEN = 19
+const board = Array(BOARD_LEN)
+  .fill(0)
+  .map(() => Array(BOARD_LEN).fill(0));
 
 //https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,13 +33,44 @@ const db = getFirestore(app);
 // Initialize Cloud Functions and get ref to the service
 //const functions = getFunctions(app);
 
+
+const generateUUID = () => {
+  return uuidv4(); // ⇨ ex) '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+}
+
+// func to create a board object to store in firebase (firebase does not support nested arrays)
+const createBoardObject = (board, BOARD_LEN) => {
+
+  const object = {};
+
+  // quick and simple  algo to construct object from 2-d array
+  // loop both arrays -> create key -> store original value into new object's hashmap 
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const key = `${i}-${j}`;
+      object[key] = board[i][j];
+    }
+  }
+  return object;
+}
+
+const boardObject = createBoardObject(board, BOARD_LEN);
+
 const createGame = async () => {
   try {
+
+    // assign game unique id
+    const uuid = generateUUID();
+
+    // add a single game (document) in 'games' collection 
     const docRef = await addDoc(collection(db, "games"), {
-      board_uid: "abc",
-      board: [0, 1, 2],
+      board_uid: uuid,
+      board: boardObject,
     });
+
     console.log("Document written with ID: ", docRef.id);
+    //console.log("board-object is: ", board_obj);
+
   } catch (e) {
     console.error("Error adding document: ", e);
   }
